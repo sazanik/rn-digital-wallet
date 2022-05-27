a5cb37309c43daf199fd9359cde7d1797f86aad8
Mikita Sazanchuk <mikit.sazan@gmail.com>
2022-05-27 21:18:26 +0300

    feat: add part content for Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { colors } from '../../constants/colors';
+import { AddSVG } from '../../assets/SVGs/Add';
 
 interface Props {
   name: string;
@@ -17,11 +18,20 @@ export const Card = ({ name, initialBalance = 0 }: Props) => {
       start={{ x: 0, y: 0.5 }}
       end={{ x: 1, y: 0.5 }}>
       <View style={styles.row}>
-        <Text style={styles.name}>{name}</Text>
+        <Text style={[styles.text, styles.name]}>{name}</Text>
+        <AddSVG />
       </View>
-      <View />
-      <View />
-      <View />
+      <View style={styles.row}>
+        <Text style={[styles.text, styles.balance]}>
+          {`$ ${initialBalance}`}
+        </Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={[styles.text, styles.number]}>12** **** **** 3456</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={[styles.text, styles.expireDate]}>01/23</Text>
+      </View>
     </LinearGradient>
   );
 };
@@ -34,12 +44,33 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
   row: {
+    flexDirection: 'row',
     justifyContent: 'space-between',
   },
+  text: {
+    color: colors.white,
+  },
   name: {
-    color: colors.while,
-    fontWeight: '500',
+    marginBottom: 8,
+    fontWeight: '600',
+    fontSize: 18,
+    lineHeight: 24,
+  },
+  balance: {
+    marginBottom: 36,
+    fontWeight: '600',
+    fontSize: 32,
+    lineHeight: 40,
+  },
+  number: {
+    marginBottom: 4,
+    fontWeight: '600',
     fontSize: 16,
     lineHeight: 24,
+  },
+  expireDate: {
+    fontWeight: '400',
+    fontSize: 16,
+    lineHeight: 20,
   },
 });
--- src/constants/colors/index.ts
+++ src/constants/colors/index.ts
@@ -1,6 +1,6 @@
 export const colors = {
   black: '#2E2D30',
-  while: '#ffffff',
+  white: '#ffffff',
   lightBlueGradient: '#3CA6D3',
   darkBlueGradient: '#644CC4',
 };
